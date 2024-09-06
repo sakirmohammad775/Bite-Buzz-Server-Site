943094e619174ad002710860f0e67b7f61529571
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-09-06 23:10:58 +0600

    mongod db connect

--- index.js
+++ index.js
@@ -35,7 +35,9 @@ async function run() {
 }
 run().catch(console.dir);
 
-
+app.get('/',(req,res)=>{
+    res.send('bite-buzz is formatting')
+})
 app.listen (port,()=>{
     console.log(`bite-buzz is formatting ${port}`)
 })
