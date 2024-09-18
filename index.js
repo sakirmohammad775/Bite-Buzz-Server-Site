eb3abd171777afeea2807d102ff80dac717418d7
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-09-18 12:44:48 +0600

    delete the cart item set

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const cors = require('cors')
 require('dotenv').config()
 const port = process.env.PORT || 5000
 
+
 //middleware
 app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mfnurby.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -47,12 +48,17 @@ async function run() {
     })
     app.get('/carts', async (req, res) => {
       const email = req.query.email; // Capture email from query string
-      console.log('Email from query:', email);
       const query = { email: email };
       const result = await cartCollection.find(query).toArray();
       res.send(result);
-      
+
     });
+    app.delete('/carts/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result=await cartCollection.deleteOne(query)
+      res.send(result)
+    })
 
 
     // Send a ping to confirm a successful connection
