caf20cde2e55458f1772e3d8f70cb109a16371de
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-09-21 19:45:18 +0600

    add google signIn

--- index.js
+++ index.js
@@ -27,9 +27,29 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const userCollection = client.db("Bite-Buzz").collection('users')
     const menuCollection = client.db("Bite-Buzz").collection('menu')
     const reviewCollection = client.db("Bite-Buzz").collection('reviews')
     const cartCollection = client.db("Bite-Buzz").collection('carts')
+
+    //users related api
+    app.get('/users',async(req,res)=>{
+      const result=await userCollection.find().toArray()
+      res.send(result)
+    })
+
+    app.post('/users', async (req, res) => {
+      const user = req.body
+     //insert email if user doesn't exists
+     //you can do this many ways (1.email unique 2.upsert 3.simple checking)
+     const query ={email:user.email}
+     const existingUser=await userCollection.findOne(query)
+     if(existingUser){
+      return res.send({message:'user already exists',insertId:null})
+     }
+      const result = await userCollection.insertOne(user)
+      res.send(result)
+    })
 
     //get data from server site
     app.get('/menu', async (req, res) => {
@@ -56,7 +76,7 @@ async function run() {
     app.delete('/carts/:id', async (req, res) => {
       const id = req.params.id
       const query = { _id: new ObjectId(id) }
-      const result=await cartCollection.deleteOne(query)
+      const result = await cartCollection.deleteOne(query)
       res.send(result)
     })
 
