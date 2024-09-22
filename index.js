31a2076c4aaef36e2c3b4f90003994527b8b02c9
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-09-22 21:13:52 +0600

    add all users

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const jwt=require('jsonwebtoken')
 require('dotenv').config()
 const port = process.env.PORT || 5000
 
@@ -31,6 +32,14 @@ async function run() {
     const menuCollection = client.db("Bite-Buzz").collection('menu')
     const reviewCollection = client.db("Bite-Buzz").collection('reviews')
     const cartCollection = client.db("Bite-Buzz").collection('carts')
+
+    //jwt related api
+    app.post('/jwt',async(req,res)=>{
+      const user=req.body;
+      const token=jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{
+        expiresIn:'1h'})
+        res.send(token)
+    })
 
     //users related api
     app.get('/users', async (req, res) => {
