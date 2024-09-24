6307261c0e10208adddb2c3c093a64d121bd325a
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-09-24 22:31:00 +0600

    add the new feature intercepter

--- index.js
+++ index.js
@@ -49,8 +49,8 @@ async function run() {
         return res.status(401).send({ message: 'forbidden access' })
       }
       const token = req.headers.authorization.split(' ')[1] //check authentication bearer [0] [1] format
-      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
-        if (error) {
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
           return res.status(401).send({ message: 'forbidden access' })
         }
         req.decoded = decoded;
