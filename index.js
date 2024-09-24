8dd4545f9eede18d9e4d96913a017938efd35c3c
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-09-24 23:58:47 +0600

    Make user api secure using verify Admin

--- index.js
+++ index.js
@@ -58,9 +58,20 @@ async function run() {
       })
 
     }
+    //use verify admin after verifyToken
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email
+      const query = { email: email }
+      const user = await userCollection.findOne(query)
+      const isAdmin = user?.role === 'admin';
+      if (!isAdmin) {
+        return res.status(403).send({ message: "forbidden access" })
+      }
+      next()
+    }
 
     //users related api
-    app.get('/users', verifyToken, async (req, res) => {
+    app.get('/users', verifyToken, verifyAdmin,async (req, res) => {
       console.log(req.headers)
       const result = await userCollection.find().toArray()
       res.send(result)
@@ -94,14 +105,14 @@ async function run() {
       res.send(result)
     })
 
-    app.delete('/users/:id', async (req, res) => {
+    app.delete('/users/:id',verifyAdmin,verifyToken, async (req, res) => {
       const id = req.params.id
       const query = { _id: new ObjectId(id) }
       const result = await userCollection.deleteOne(query)
       res.send(result)
     })
 
-    app.patch('/users/admin/:id', async (req, res) => {
+    app.patch('/users/admin/:id',verifyToken,verifyToken, async (req, res) => {
       const id = req.params.id
       const filter = { _id: new ObjectId(id) }
       const updatedDoc = {
