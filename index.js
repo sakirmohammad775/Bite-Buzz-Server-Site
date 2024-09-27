d2de2897779688c00aaadf1892ed60dee2ab95ab
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-09-27 22:16:38 +0600

    use interceptor

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ async function run() {
       if (!isAdmin) {
         return res.status(403).send({ message: "forbidden access" })
       }
-      console.log(object)
+      
       next()
     }
 
