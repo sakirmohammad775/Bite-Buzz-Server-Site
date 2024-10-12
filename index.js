1c57b68abcdc08bac20bcc59347b3ebcb7904f7c
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2024-10-12 15:59:26 +0600

    get and post on

--- index.js
+++ index.js
@@ -133,6 +133,13 @@ async function run() {
       const result = await menuCollection.find().toArray()
       res.send(result)
     })
+    
+    app.get('/menu/:id',async(req,res)=>{
+      const id=req.params.id
+      const query={_id:new ObjectId(id)}
+      const result=await menuCollection.findOne(query)
+      res.send(result)
+    })
 
     app.post('/menu', verifyToken, verifyAdmin, async (req, res) => {
       const item = req.body;
